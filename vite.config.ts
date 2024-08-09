8264e7c2c4aab049deb042c87b8d125a4546dbdd
Honin <honin@honi-lith>
2024-08-09 12:57:08 +0200

    Once More

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'VirtualGallery/',
+  base: 'https://darkhonin.github.io/VirtualGallery/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
